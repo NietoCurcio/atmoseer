274f7b777020b5989d5c3fe7e5c1c12f3157e94e
Eduardo <edubezerra@gmail.com>
2023-04-24 07:18:00 -0300

    Change prefix: import --> retrieve

--- src/retrieve_sounding.py
+++ src/retrieve_sounding.py
@@ -1,16 +1,18 @@
 '''
     This script provides a simple interface for retrieving upper air sounding 
-    data from the University of Wyoming Upper Air Archive. 
+    observations from the University of Wyoming Upper Air Archive. 
     
     Its code uses the WyomingUpperAir class in the siphon library. In particular, 
-    the request_data method of the WyomingUpperAir class takes two arguments: the 
-    date of the sounding as a datetime object, and the station ID as a string.
+    the request_data method of the WyomingUpperAir class takes two arguments: 
+     - the date of the sounding launch as a datetime object, 
+     - the station ID as a string.
     
     Here's an example of how to use this script:
     python import_sounding.py --station_id SBGL --start_year 2023 --end_year 2023 
 '''
 import pandas as pd
-import sys, getopt
+import sys
+import argparse
 from datetime import datetime, timedelta
 from siphon.simplewebservice.wyoming import WyomingUpperAir
 import util as util
@@ -25,13 +27,12 @@ def get_data_for_year_and_hour_of_day(station_id, first_day, last_day, hour_of_d
 
     while next_day <= last_day:
 
-        current_launch = next_day + timedelta(hours=hour_of_day)
+        current_launch_time = next_day + timedelta(hours=hour_of_day)
         try:
-            print(f"Downloading data for {current_launch}...", end = "")
-            df_launch = WyomingUpperAir.request_data(current_launch, station_id)
-            print(f"Success! ({len(df_launch)} measurements).")
+            print(f"Downloading observations made on {current_launch_time}...", end = "")
+            df_launch = WyomingUpperAir.request_data(current_launch_time, station_id)
+            print(f"Success! ({len(df_launch)} observations).")
             df_all_launchs = pd.concat(([df_all_launchs, df_launch]))
-            # print(df_all_launchs.shape)
         except IndexError as e:
             print(f'{repr(e)}')
             unsuccesfull_launchs += 1
@@ -40,7 +41,7 @@ def get_data_for_year_and_hour_of_day(station_id, first_day, last_day, hour_of_d
             unsuccesfull_launchs += 1
         except requests.HTTPError as e:
             print(f'{repr(e)}')
-            print(f"Server seems to be busy while trying to download data for {current_launch}. Going to sleep for a while...", end="")
+            print(f"Server seems to be busy while trying to download data for {current_launch_time}. Going to sleep for a while...", end="")
             time.sleep(10)  # Sleep for a moment
             print("Back to work!")
             continue
@@ -57,7 +58,7 @@ def get_data_for_year_and_hour_of_day(station_id, first_day, last_day, hour_of_d
     return df_all_launchs, unsuccesfull_launchs
 
 def get_data(station_id, start_year, end_year):
-    print(f"Downloading observations from radiosonde {station_id}...")
+    print(f"Downloading observations from sounding station {station_id}...")
     df_all_launchs = pd.DataFrame()
     unsuccesfull_launchs = 0
     end_year = min([end_year, datetime.now().year])
@@ -66,25 +67,31 @@ def get_data(station_id, start_year, end_year):
 
     for year in range(start_year, end_year+1):
         first_day, last_day = util.get_first_and_last_days_of_year(year)
+        
+        # Download data at 00Z
         df_launchs, nb_unsuccessful_launchs_of_year = get_data_for_year_and_hour_of_day(station_id, first_day, last_day, 0)
         df_all_launchs = pd.concat(([df_all_launchs, df_launchs]))
         unsuccesfull_launchs += nb_unsuccessful_launchs_of_year
+
+        # Download data at 12Z
         df_launchs, nb_unsuccessful_launchs_of_year = get_data_for_year_and_hour_of_day(station_id, first_day, last_day, 12)
         df_all_launchs = pd.concat(([df_all_launchs, df_launchs]))
         unsuccesfull_launchs += nb_unsuccessful_launchs_of_year
+
     print(f"Done! There were {unsuccesfull_launchs} unsuccessful launchs in the specified period.")
     filename = '../data/sounding/' + station_id + '_'+ str(start_year) + '_' + str(end_year) + '.csv'
-    print(f"Saving data on {df_all_launchs.shape[0]} measurements to file {filename}.", end=" ")
+    print(f"Saving data on {df_all_launchs.shape[0]} observations to file {filename}.", end=" ")
     df_all_launchs.to_parquet(filename, compression='gzip', index = False)
     print("Done!")
-
-import argparse
 
 def main(argv):
 
     sounding_station_id = 'SBGL'
 
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        description="""This script provides a simple interface for retrieving upper air
+        sounding observations made by a user-provided station from the University of Wyoming Upper Air Archive.""",
+        prog=sys.argv[0])
     parser.add_argument('-s', '--station_id', help='Sounding station ID', default='SBGL')
     parser.add_argument('-b', '--start_year', help='Start year', required=True)
     parser.add_argument('-e', '--end_year', help='End year', required=True)
