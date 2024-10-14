1427c52f6f0c6409c33828acdc823ba96516b6eb
Felipe Curcio <felipe_nieto010@hotmail.com>
2024-10-14 18:04:19 -0300

    total_precipitation single levels download

--- src/retrieve_ERA5.py
+++ src/retrieve_ERA5.py
@@ -3,40 +3,39 @@ import argparse
 from pathlib import Path
 from datetime import datetime
 from collections.abc import Generator
-from typing import Union
 
-from cdsapi.api import Client as ClientCDS
+import cdsapi
 import xarray as xr
 from xarray.core.dataset import Dataset
 
 import globals
 
 """
-For using the CDS API to download ERA-5 data consult: https://cds.climate.copernicus.eu/api-how-to
+For using the CDS API to download ERA-5 data consult: https://cds.climate.copernicus.eu/how-to-api
 """
 
 REGION_OF_INTEREST = {'north': -22, 'west': -44, 'south': -23, 'east': -42}
 
 class DatasetClient:
     def __init__(self) -> None:
-        self.clientCDS = ClientCDS(timeout=30, retry_max=5, sleep_max=30)
+        self.clientCDS = cdsapi.Client(timeout=30, retry_max=5, sleep_max=30)
 
     def _convert_grib_to_netcdf(self, target: str):
         # please see, https://github.com/ecmwf/cfgrib
         # also, https://docs.xarray.dev/en/stable/user-guide/io.html#grib-format-via-cfgrib
-        print(f"Converting grib data to netcdf...")
+        print("Converting grib data to netcdf...")
         assert target.endswith('.grib'), "The target file must be a grib file"
         data = xr.open_dataset(target, engine='cfgrib')
         target = target.replace('.grib', '.nc')
         data.to_netcdf(target)
-        print(f"Converted grib data to netcdf")
+        print("Converted grib data to netcdf")
 
     def call_retrieve(self, name: str, request: dict, target: str):
         # Important note: this commit removing grib format download is jsut a test
         # it does not mean that we will not use grib format any time if needed
         # it also does not mean that the conversion from grib to netcdf is causing trouble
         # needs further investigation
-        MAX_RETRIES = 5
+        MAX_RETRIES = 1
         for i in range(MAX_RETRIES):
             try:
                 self.clientCDS.retrieve(name=name, request=request, target=target)
@@ -51,10 +50,14 @@ class DatasetClient:
                 # print(f"Failed to download ERA5 data in netcdf format. Downloading in grib format")
                 # request['format'] = 'grib'
                 # target = target.replace('.nc', '.grib')
-                print(f"Failed to download ERA5 data in netcdf format.")
+                print("Failed to download ERA5 data in netcdf format.")
                 print(f"Error message: {e} - {repr(e)}")
                 print(f"{i + 1}/{MAX_RETRIES} retries")
-                self.call_retrieve(name, request, target)
+
+                if i == MAX_RETRIES - 1:
+                    raise e
+
+                # self.call_retrieve(name, request, target)
 
 class CDSDatasetDownloader:
     def __init__(self, begin_year: int, begin_month: int, end_year: int, end_month: int) -> None:
@@ -64,10 +67,10 @@ class CDSDatasetDownloader:
         self.end_month = end_month
         self.dataset_client = DatasetClient()
 
-    def _get_dates_generator(self) -> Generator[str, None, None]:
+    def _get_dates_generator(self) -> Generator[tuple[int, int], None, None]:
         current_date = datetime.strptime(f"{self.begin_year}-{self.begin_month}", "%Y-%m")
-        end_date = datetime.strptime(f"{self.end_year}-{self.end_month + 1}", "%Y-%m")
-        while current_date < end_date:
+        end_date = datetime.strptime(f"{self.end_year}-{self.end_month }", "%Y-%m")
+        while current_date <= end_date:
             year = int(current_date.year)
             month = int(current_date.month)
 
@@ -82,30 +85,30 @@ class CDSDatasetDownloader:
         for year, month in self._get_dates_generator():
             yield xr.open_dataset(f"{globals.NWP_DATA_DIR}ERA5/montly_data/RJ_{year}_{month}.nc")
 
-    def _download_dataset(self, month: str, year: str):
+    def _download_dataset(self, month: int, year: int):
         target_path_nc = Path(f"{globals.NWP_DATA_DIR}ERA5/montly_data/RJ_{year}_{month}.nc")
         if target_path_nc.is_file():
             print(f"ERA5 data already downloaded for the month {month} of year {year}")
             return
-        
-        target_path_grib = Path(f"{globals.NWP_DATA_DIR}ERA5/montly_data/RJ_{year}_{month}.grib")
-        if target_path_grib.is_file():
-            print(f"ERA5 data already downloaded for the month {month} of year {year} with GRIB format")
-            self.dataset_client._convert_grib_to_netcdf(str(target_path_grib.resolve()))
-            return
+        if not target_path_nc.parent.is_dir():
+            print(f"Creating directory {target_path_nc}")
+            target_path_nc.parent.mkdir(parents=True)
         
         request = {
-            "product_type": "reanalysis",
+            "product_type": ["reanalysis"],
             "format": "netcdf",
             "variable": [
-                "10m_u_component_of_wind", "10m_v_component_of_wind", "2m_dewpoint_temperature",
-                "2m_temperature", "convective_available_potential_energy", "convective_inhibition",
-                "geopotential", "k_index", "total_totals_index",
+                # "10m_u_component_of_wind", "10m_v_component_of_wind", "2m_dewpoint_temperature",
+                # "2m_temperature", "convective_available_potential_energy", "convective_inhibition",
+                # "geopotential", "k_index", "total_totals_index",
+                "total_precipitation"
             ],
-            "year": year,
+            "year": [year],
             "month": [month],
             "day": [f"{day:02d}" for day in range(1, 32)],
             "time": [f"{hour:02d}:00" for hour in range(24)],
+            "data_format": "netcdf",
+            "download_format": "unarchived",
             "area": [REGION_OF_INTEREST[key] for key in ['north', 'west', 'south', 'east']]
         }
 
@@ -122,6 +125,32 @@ class CDSDatasetDownloader:
         for year, month in self._get_dates_generator():
             self._download_dataset(month, year)
         print(f"Downloaded ERA5 data for the period {self.begin_year} to {self.end_year}")
+    
+    def check_datasets(self):
+        # check if all datasets were downloaded
+        # to do so:
+        # check if the number of files in the directory is equal to the number of months between the begin and end dates
+        # check if the files are named RJ_YYYY_MM.nc from the begin to the end date:
+
+        target_dir = Path(f"{globals.NWP_DATA_DIR}ERA5/montly_data")
+
+        if not target_dir.is_dir():
+            raise FileNotFoundError(f"Directory not found: {target_dir}")
+        
+        downloaded_files = list(target_dir.glob("*.nc"))
+
+        expected_files = []
+
+        for year, month in self._get_dates_generator():
+            expected_files.append(f"RJ_{year}_{month}.nc")
+
+        if len(downloaded_files) != len(expected_files):
+            missing_files = set(expected_files) - set([file.name for file in downloaded_files])
+            print(f"Missing files: {missing_files}")
+            raise FileNotFoundError("Not all datasets were downloaded")
+
+        print("All datasets were downloaded")
+
 
     def prepend_dataset(self, prepend_dataset: str):
         if not Path(prepend_dataset).is_file():
@@ -204,6 +233,16 @@ def main(argv):
     REGION_OF_INTEREST['south'] = args.south
     REGION_OF_INTEREST['east'] = args.east
 
+    print(f"""
+        Config:
+        Begin year: {begin_year}
+        Begin month: {begin_month}
+        End year: {end_year}
+        End month: {end_month}
+        Prepend dataset: {prepend_dataset}
+        Region of interest: {REGION_OF_INTEREST}
+    """)
+
     # ERA5 data goes back to the year 1940. 
     # see https://cds.climate.copernicus.eu/cdsapp#!/dataset/reanalysis-era5-single-levels?tab=form
     assert begin_year >= 1940, "ERA5 start year must be greater than or equal to 1940"
@@ -216,7 +255,10 @@ def main(argv):
         end_month=end_month
     )
     dataset_downloader.download_datasets()
-    dataset_downloader.merge_datasets()
+
+    dataset_downloader.check_datasets()
+
+    # dataset_downloader.merge_datasets()
     if prepend_dataset:
         dataset_downloader.prepend_dataset(prepend_dataset)
 
