6bbdeb502ad969652a9870487acbea50ffd0135e
Felipe Curcio <felipe_nieto010@hotmail.com>
2024-11-05 18:20:09 -0300

    update for 19 features - tp + (t, rh, u, v, spd, w at 200, 700 and 1000 pressure levels)

--- src/spatiotemporal_builder/WebsirenesTarget.py
+++ src/spatiotemporal_builder/WebsirenesTarget.py
@@ -17,28 +17,25 @@ from .WebSirenesSquare import WebSirenesSquare, websirenes_square
 log = logger.get_logger(__name__)
 
 
-class WebsirenesTarget:
+class SpatioTemporalFeatures:
     def __init__(
         self,
         websirenes_parser: WebSirenesParser,
         websirenes_square: WebSirenesSquare,
     ):
-        self.websirenes_target_path = Path(__file__).parent / "target"
-        if not self.websirenes_target_path.exists():
-            self.websirenes_target_path.mkdir()
-
         self.websirenes_features_path = Path(__file__).parent / "features"
         if not self.websirenes_features_path.exists():
             self.websirenes_features_path.mkdir()
 
-        self.era5land_path = Path(__file__).parent.parent.parent / "data/reanalysis/ERA5Land"
-        if not self.era5land_path.exists():
-            log.error(
-                f"ERA5Land folder not found in {self.era5land_path}. Please place the ERA5Land dataset in the expected folder"
-            )
-            exit(1)
+        self.era5_single_levels_path = (
+            Path(__file__).parent.parent.parent / "data/reanalysis/ERA5-single-levels"
+        )
+        self.era5_pressure_levels_path = (
+            Path(__file__).parent.parent.parent / "data/reanalysis/ERA5-pressure-levels"
+        )
 
-        self.current_ds = None
+        self.current_single_levels_ds = None
+        self.current_pressure_levels_ds = None
         self.current_year_month = None
         self.websirenes_parser = websirenes_parser
         self.websirenes_square = websirenes_square
@@ -47,10 +44,12 @@ class WebsirenesTarget:
 
         self.sorted_latitudes_ascending = np.sort(lats)
         self.sorted_longitudes_ascending = np.sort(lons)
-
-    def _write_target(self, target: npt.NDArray[np.float64], timestamp: pd.Timestamp):
-        target_filename = self.websirenes_target_path / f"{timestamp.strftime('%Y_%m_%d_%H')}.npy"
-        np.save(target_filename, target)
+        log.debug("SpatioTemporalFeatures initialized")
+        log.debug(f"sorted_latitudes_ascending: {self.sorted_latitudes_ascending}")
+        log.debug(f"sorted_longitudes_ascending: {self.sorted_longitudes_ascending}")
+        log.info(
+            f"Spatial resolution: {self.sorted_latitudes_ascending[1] - self.sorted_latitudes_ascending[0]:.2f} degrees"
+        )
 
     def _write_features(self, features: npt.NDArray[np.float64], timestamp: pd.Timestamp):
         features_filename = (
@@ -59,48 +58,75 @@ class WebsirenesTarget:
         np.save(features_filename, features)
 
     def _get_grid_lats_lons(self) -> tuple[npt.NDArray[np.float32], npt.NDArray[np.float32]]:
-        ds = self._get_era5land_dataset(2022, 1)
+        ds = self._get_era5_pressure_levels_dataset(2009, 6)
         lats = ds.coords["latitude"].values
         lons = ds.coords["longitude"].values
         return lats, lons
 
-    def _get_era5land_dataset(self, year: int, month: int) -> xr.Dataset:
-        if self.current_year_month == (year, month) and self.current_ds is not None:
-            return self.current_ds
+    def _get_era5_single_levels_dataset(self, year: int, month: int) -> xr.Dataset:
+        if self.current_year_month == (year, month) and self.current_single_levels_ds is not None:
+            return self.current_single_levels_ds
 
-        era5land_year_month_path = self.era5land_path / "monthly_data" / f"RJ_{year}_{month}.nc"
-
-        if not os.path.exists(era5land_year_month_path):
-            raise FileNotFoundError(f"File {era5land_year_month_path} not found")
-
-        ds = xr.open_dataset(era5land_year_month_path)
-        if "expver" in list(ds.coords.keys()):
-            log.warning(">>>Oops! expver dimension found. Going to remove it.<<<")
-            ds_combine = ds.sel(expver=1).combine_first(ds.sel(expver=5))
-            ds_combine.load()
-            ds = ds_combine
-        ds = ds[["u10", "v10", "d2m", "t2m", "sp", "tp"]]
-        self.current_ds = ds
-        self.current_year_month = (year, month)
-        return self.current_ds
-
-    def _get_relative_humidity(self, temperature: np.float64, dew_point: np.float64):
-        return (
-            100
-            * np.exp((17.625 * dew_point) / (243.04 + dew_point))
-            / np.exp((17.625 * temperature) / (243.04 + temperature))
+        era5_year_month_path = (
+            self.era5_single_levels_path / "monthly_data" / f"RJ_{year}_{month}.nc"
         )
+
+        if not os.path.exists(era5_year_month_path):
+            raise FileNotFoundError(f"File {era5_year_month_path} not found")
+
+        ds = xr.open_dataset(era5_year_month_path)
+        # new version of ERA5 causes error in the code below
+        # if "expver" in list(ds.coords.keys()):
+        #     log.warning(">>>Oops! expver dimension found. Going to remove it.<<<")
+        #     ds.sel(expver="0001", method="nearest")
+        #     ds_combine = ds.sel(expver=1).combine_first(ds.sel(expver=5))
+        #     exit(0)
+        #     ds_combine.load()
+        #     ds = ds_combine
+        ds = ds[["tp"]]
+        self.current_single_levels_ds = ds
+        self.current_year_month = (year, month)
+        return self.current_single_levels_ds
+
+    def _get_era5_pressure_levels_dataset(self, year: int, month: int) -> xr.Dataset:
+        if self.current_year_month == (year, month) and self.current_pressure_levels_ds is not None:
+            return self.current_pressure_levels_ds
+
+        era5_year_month_path = (
+            self.era5_pressure_levels_path / "monthly_data" / f"RJ_{year}_{month}.nc"
+        )
+
+        if not os.path.exists(era5_year_month_path):
+            raise FileNotFoundError(f"File {era5_year_month_path} not found")
+
+        ds = xr.open_dataset(era5_year_month_path)
+        # new version of ERA5 causes error in the code below
+        # if "expver" in list(ds.coords.keys()):
+        #     log.warning(">>>Oops! expver dimension found. Going to remove it.<<<")
+        #     ds_combine = ds.sel(expver=1).combine_first(ds.sel(expver=5))
+        #     ds_combine.load()
+        #     ds = ds_combine
+        ds = ds[["r", "t", "u", "v", "w"]]
+        self.current_pressure_levels_ds = ds
+        self.current_year_month = (year, month)
+        return self.current_pressure_levels_ds
 
     def _process_grid(
         self,
         features: npt.NDArray[np.float64],
-        target: npt.NDArray[np.float64],
-        ds_time: xr.Dataset,
+        ds_single_levels: xr.Dataset,
+        ds_pressure_levels: xr.Dataset,
         timestamp: pd.Timestamp,
     ):
         top_down_lats = self.sorted_latitudes_ascending[::-1]
         left_right_lons = self.sorted_longitudes_ascending
-        for i, lat in enumerate(top_down_lats):
+        #
+        for i, lat in tqdm(
+            enumerate(top_down_lats),
+            desc="Processing grid",
+            leave=False,
+            total=self.sorted_latitudes_ascending.size,
+        ):
             for j, lon in enumerate(left_right_lons):
                 square = self.websirenes_square.get_square(
                     lat, lon, self.sorted_latitudes_ascending, self.sorted_longitudes_ascending
@@ -111,19 +137,52 @@ class WebsirenesTarget:
 
                 websirene_keys = self.websirenes_square.get_keys_in_square(square)
 
-                precipitation_in_square = self.websirenes_square.get_precipitation_in_square(
-                    square, websirene_keys, timestamp, ds_time
+                tp = self.websirenes_square.get_precipitation_in_square(
+                    square, websirene_keys, timestamp, ds_single_levels
                 )
 
-                target[i, j] = precipitation_in_square
+                r1000, r700, r200 = self.websirenes_square.get_relative_humidity_in_square(
+                    square, ds_pressure_levels
+                )
+                t1000, t700, t200 = self.websirenes_square.get_temperature_in_square(
+                    square, ds_pressure_levels
+                )
+                u1000, u700, u200 = self.websirenes_square.get_u_component_in_square(
+                    square, ds_pressure_levels
+                )
 
-                # u10, v10, t2m, sp, d2m = self.websirenes_square.get_features_in_square(
-                #     square, ds_time
-                # )
-                # spd10 = np.sqrt(u10**2 + v10**2)
-                # rh = self._get_relative_humidity(t2m, d2m)
+                v1000, v700, v200 = self.websirenes_square.get_v_component_in_square(
+                    square, ds_pressure_levels
+                )
+                w1000, w700, w200 = self.websirenes_square.get_w_component_in_square(
+                    square, ds_pressure_levels
+                )
 
-                # features[i, j] = [u10, v10, spd10, t2m, sp, rh, precipitation_in_square]
+                speed200 = np.sqrt(u200**2 + v200**2)
+                speed700 = np.sqrt(u700**2 + v700**2)
+                speed1000 = np.sqrt(u1000**2 + v1000**2)
+
+                features[i, j] = [
+                    tp,
+                    r200,
+                    r700,
+                    r1000,
+                    t200,
+                    t700,
+                    t1000,
+                    u200,
+                    u700,
+                    u1000,
+                    v200,
+                    v700,
+                    v1000,
+                    speed200,
+                    speed700,
+                    speed1000,
+                    w200,
+                    w700,
+                    w1000,
+                ]
 
     def _process_timestamp(self, timestamp: pd.Timestamp):
         year = timestamp.year
@@ -131,23 +190,34 @@ class WebsirenesTarget:
         day = timestamp.day
         hour = timestamp.hour
 
-        ds = self._get_era5land_dataset(year, month)
+        ds_single_levels = self._get_era5_single_levels_dataset(year, month)
+        ds_pressure_levels = self._get_era5_pressure_levels_dataset(year, month)
 
         time = f"{year}-{month}-{day}T{hour}:00:00.000000000"
-        ds_time = ds.sel(time=time, method="nearest")
-        target = np.zeros(
-            (self.sorted_latitudes_ascending.size, self.sorted_longitudes_ascending.size),
-            dtype=np.float64,
-        )
+
+        ds_single_levels_time = ds_single_levels.sel(valid_time=time, method="nearest")
+        ds_pressure_levels_time = ds_pressure_levels.sel(valid_time=time, method="nearest")
 
         features_tuple = {
-            "u10": "10 metre U wind component",
-            "v10": "10 metre V wind component",
-            "spd10": "10 metre wind speed",
-            "t2m": "2 metre temperature",
-            "sp": "Surface pressure",
-            "humidty": "Relative humidity from temp and dew point",
             "tp": "Total precipitation",
+            "r200": "Relative humidity at 200 hPa",
+            "r700": "Relative humidity at 700 hPa",
+            "r1000": "Relative humidity at 1000 hPa",
+            "t200": "Temperature at 200 hPa",
+            "t700": "Temperature at 700 hPa",
+            "t1000": "Temperature at 1000 hPa",
+            "u200": "U component of wind",
+            "u700": "U component of wind",
+            "u1000": "U component of wind",
+            "v200": "V component of wind",
+            "v700": "V component of wind",
+            "v1000": "V component of wind",
+            "speed200": "Speed of wind at 200 hPa",
+            "speed700": "Speed of wind at 700 hPa",
+            "speed1000": "Speed of wind at 1000 hPa",
+            "w200": "Vertical velocity at 200 hPa",
+            "w700": "Vertical velocity at 700 hPa",
+            "w1000": "Vertical velocity at 1000 hPa",
         }
         features = np.zeros(
             (
@@ -158,19 +228,19 @@ class WebsirenesTarget:
             dtype=np.float64,
         )
 
-        self._process_grid(features, target, ds_time, timestamp)
-        self._write_target(target, timestamp)
-        # self._write_features(features, timestamp)
+        self._process_grid(features, ds_single_levels_time, ds_pressure_levels_time, timestamp)
+        self._write_features(features, timestamp)
 
     def build_timestamps_hourly(
         self,
         start_date: Optional[pd.Timestamp],
         end_date: Optional[pd.Timestamp],
+        ignored_months: list[int],
         use_cache: bool = True,
     ):
-        if use_cache and len(list(self.websirenes_target_path.glob("*.npy"))) > 0:
+        if use_cache and len(list(self.websirenes_features_path.glob("*.npy"))) > 0:
             log.warning(
-                f"Using cached target. To clear cache delete the {self.websirenes_target_path} folder"
+                f"Using cached target. To clear cache delete the {self.websirenes_features_path} folder"
             )
             return
 
@@ -184,43 +254,61 @@ class WebsirenesTarget:
 
         log.info(f"Building websirenes target from {timestamps[0]} to {timestamps[-1]}")
         start_time = time.time()
-        FIVE_MINUTES = 60 * 5
+        THREE_MINUTES = 60 * 3
         with concurrent.futures.ProcessPoolExecutor() as executor:
-            futures = [
-                executor.submit(self._process_timestamp, timestamp) for timestamp in timestamps
-            ]
+            futures = []
+
+            for timestamp in timestamps:
+                if self.websirenes_features_path.joinpath(
+                    f"{timestamp.strftime('%Y_%m_%d_%H')}_features.npy"
+                ).exists():
+                    continue
+
+                if timestamp.month in ignored_months:
+                    continue
+
+                futures.append(executor.submit(self._process_timestamp, timestamp))
+
             pbar = tqdm(
                 total=len(timestamps),
                 desc="Processing timestamps",
                 file=TqdmLogger(log),
                 dynamic_ncols=True,
-                mininterval=FIVE_MINUTES,
+                mininterval=THREE_MINUTES,
             )
             for _ in concurrent.futures.as_completed(futures):
                 pbar.update()
             pbar.close()
         # multiprocessing version took 22.98s 2011-04-12T21:00:00 2011-04-13T10:00:00
-        """
-        for i in tqdm(
-            range(len(timestamps)),
-            desc="Processing timestamps",
-            file=TqdmLogger(log),
-            dynamic_ncols=True,
-            mininterval=FIVE_MINUTES,
-        ):
-            self._process_timestamp(timestamps[i])
-        sequential/synchronous version took 62.7s 2011-04-12T21:00:00 2011-04-13T10:00:00
-        """
+        # for i in tqdm(
+        #     range(len(timestamps)),
+        #     desc="Processing timestamps",
+        #     file=TqdmLogger(log),
+        #     dynamic_ncols=True,
+        #     mininterval=THREE_MINUTES,
+        # ):
+        #     if self.websirenes_features_path.joinpath(
+        #         f"{timestamps[i].strftime('%Y_%m_%d_%H')}_features.npy"
+        #     ).exists():
+        #         continue
+
+        #     if timestamps[i].month in ignored_months:
+        #         continue
+
+        #     self._process_timestamp(timestamps[i])
+        # sequential/synchronous version took 62.7s 2011-04-12T21:00:00 2011-04-13T10:00:00
         end_time = time.time()
         log.info(f"Target built in {end_time - start_time:.2f} seconds")
 
-        if len(list(self.websirenes_target_path.glob("*.npy"))) != len(timestamps):
+        if len(list(self.websirenes_features_path.glob("*.npy"))) != len(timestamps):
             log.error(
-                f"Error building target. Expected {len(timestamps)} files but found {len(list(self.websirenes_target_path.glob('*.npy')))}"
+                f"Error building target. Expected {len(timestamps)} files but found {len(list(self.websirenes_features_path.glob('*.npy')))}"
             )
             exit(1)
 
-        log.success(f"Websirenes target built successfully in {self.websirenes_target_path}")
+        log.success(
+            f"Websirenes features hourly built successfully in {self.websirenes_features_path}"
+        )
 
 
-websirenes_target = WebsirenesTarget(websirenes_parser, websirenes_square)
+websirenes_target = SpatioTemporalFeatures(websirenes_parser, websirenes_square)
--- src/spatiotemporal_builder/main.py
+++ src/spatiotemporal_builder/main.py
@@ -22,13 +22,14 @@ def validate_dates(
     s_date = pd.Timestamp(start_date)
     e_date = pd.Timestamp(end_date)
 
-    MIN_START_DATE = pd.Timestamp("2011-04-12 20:30:00")
-    MAX_END_DATE = pd.Timestamp("2022-06-02 21:30:00")
+    # TODO FIX THIS
+    # MIN_START_DATE = pd.Timestamp("2011-04-12 20:30:00")
+    # MAX_END_DATE = pd.Timestamp("2022-06-02 21:30:00")
 
-    if not (MIN_START_DATE <= s_date <= e_date <= MAX_END_DATE):
-        raise ValueError(
-            f"start_date and end_date must be between {MIN_START_DATE} and {MAX_END_DATE}"
-        )
+    # if not (MIN_START_DATE <= s_date <= e_date <= MAX_END_DATE):
+    #     raise ValueError(
+    #         f"start_date and end_date must be between {MIN_START_DATE} and {MAX_END_DATE}"
+    #     )
 
     log.info(f"Building spatiotemporal data from {s_date} to {e_date}")
     return s_date, e_date
@@ -49,31 +50,47 @@ def check_data_requirements():
                 f"websirenes_coords.parquet not found in {websirenes_coords_path}. Please place the websirenes coordinates dataset in the expected folder"
             )
 
-        if not websirenes_target.era5land_path.exists():
+        if not websirenes_target.era5_pressure_levels_path.exists():
             raise FileNotFoundError(
-                f"ERA5Land folder not found in {websirenes_target.era5land_path}. Please place the ERA5Land dataset in the expected folder"
+                f"ERA5 pressure levels folder not found in {websirenes_target.era5_pressure_levels_path}. Please place the ERA5 pressure levels dataset in the expected folder"
             )
 
-        if not (websirenes_target.era5land_path / "monthly_data").exists():
+        if not websirenes_target.era5_single_levels_path.exists():
             raise FileNotFoundError(
-                f"ERA5Land monthly_data folder not found in {websirenes_target.era5land_path}. Please place the ERA5Land monthly data in the expected folder"
+                f"ERA5 single levels folder not found in {websirenes_target.era5_single_levels_path}. Please place the ERA5 single levels dataset in the expected folder"
             )
 
-        if not any((websirenes_target.era5land_path / "monthly_data").glob("*.nc")):
+        if not (websirenes_target.era5_single_levels_path / "monthly_data").exists():
             raise FileNotFoundError(
-                f"No nc files found in {websirenes_target.era5land_path / 'monthly_data'}. Please place the ERA5Land monthly data in the expected folder"
+                f"ERA5 single levels monthly_data folder not found in {websirenes_target.era5_single_levels_path}. Please place the ERA5 single levels monthly data in the expected folder"
             )
 
+        if not any((websirenes_target.era5_single_levels_path / "monthly_data").glob("*.nc")):
+            raise FileNotFoundError(
+                f"No nc files found in {websirenes_target.era5_single_levels_path / 'monthly_data'}. Please place the ERA5 single levels monthly data in the expected folder"
+            )
+
+        if not (websirenes_target.era5_pressure_levels_path / "monthly_data").exists():
+            raise FileNotFoundError(
+                f"ERA5 pressure levels monthly_data folder not found in {websirenes_target.era5_pressure_levels_path}. Please place the ERA5 pressure levels monthly data in the expected folder"
+            )
+
+        if not any((websirenes_target.era5_pressure_levels_path / "monthly_data").glob("*.nc")):
+            raise FileNotFoundError(
+                f"No nc files found in {websirenes_target.era5_pressure_levels_path / 'monthly_data'}. Please place the ERA5 pressure levels monthly data in the expected folder"
+            )
     except Exception as e:
         log.error(f"Error while checking data requirements: {e}")
         exit(1)
 
 
-def build_target(start_date: Optional[pd.Timestamp], end_date: Optional[pd.Timestamp]):
+def build_target(
+    start_date: Optional[pd.Timestamp], end_date: Optional[pd.Timestamp], ignored_months: list[int]
+):
     try:
-        websirenes_keys.build_keys(start_date, end_date)
-        websirenes_target.build_timestamps_hourly(start_date, end_date)
-        websirenes_dataset.build_netcdf()
+        websirenes_keys.build_keys()
+        websirenes_target.build_timestamps_hourly(start_date, end_date, ignored_months)
+        websirenes_dataset.build_netcdf(ignored_months)
     except Exception as e:
         log.error(f"Error while building target: {e}")
 
@@ -81,22 +98,30 @@ def build_target(start_date: Optional[pd.Timestamp], end_date: Optional[pd.Times
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Build spatiotemporal data")
     parser.add_argument(
-        "--start_date",
+        "--start-date",
         type=str,
         required=False,
         help="Start date in the format YYYY-MM-DDTHH:MM:SS",
     )
     parser.add_argument(
-        "--end_date",
+        "--end-date",
         type=str,
         required=False,
         help="End date in the format YYYY-MM-DDTHH:MM:SS",
     )
+    parser.add_argument(
+        "--ignored_months",
+        nargs="+",
+        type=int,
+        required=False,
+        default=[6, 7, 8],
+        help="Months to ignore (e.g., --ignored_months 6 7 8)",
+    )
 
-    check_data_requirements()
+    # check_data_requirements()
 
     args = parser.parse_args()
 
     start_date, end_date = validate_dates(args.start_date, args.end_date)
 
-    build_target(start_date, end_date)
+    build_target(start_date, end_date, args.ignored_months)
