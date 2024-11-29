fd9da6840eef47b8a3a1f587a26d347b790209c5
Felipe Curcio <felipe_nieto010@hotmail.com>
2024-11-29 00:23:05 -0300

    fix bottom row and right column first commit

--- src/spatiotemporal_builder/WebsirenesTarget.py
+++ src/spatiotemporal_builder/WebsirenesTarget.py
@@ -50,7 +50,8 @@ class SpatioTemporalFeatures:
 
         self.current_single_levels_ds = None
         self.current_pressure_levels_ds = None
-        self.current_year_month = None
+        self.current_year_month_pressure = None
+        self.current_year_month_single = None
 
         self.websirenes_square = websirenes_square
         self.inmet_square = inmet_square
@@ -59,6 +60,29 @@ class SpatioTemporalFeatures:
 
         self.sorted_latitudes_ascending = np.sort(lats)
         self.sorted_longitudes_ascending = np.sort(lons)
+
+        self.features_tuple = {
+            "tp": "Total precipitation",
+            "r200": "Relative humidity at 200 hPa",
+            "r700": "Relative humidity at 700 hPa",
+            "r1000": "Relative humidity at 1000 hPa",
+            "t200": "Temperature at 200 hPa",
+            "t700": "Temperature at 700 hPa",
+            "t1000": "Temperature at 1000 hPa",
+            "u200": "U component of wind",
+            "u700": "U component of wind",
+            "u1000": "U component of wind",
+            "v200": "V component of wind",
+            "v700": "V component of wind",
+            "v1000": "V component of wind",
+            "speed200": "Speed of wind at 200 hPa",
+            "speed700": "Speed of wind at 700 hPa",
+            "speed1000": "Speed of wind at 1000 hPa",
+            "w200": "Vertical velocity at 200 hPa",
+            "w700": "Vertical velocity at 700 hPa",
+            "w1000": "Vertical velocity at 1000 hPa",
+        }
+
         log.debug("SpatioTemporalFeatures initialized")
         log.debug(
             f"Grid: {len(self.sorted_latitudes_ascending)}x{len(self.sorted_longitudes_ascending)}"
@@ -80,8 +104,22 @@ class SpatioTemporalFeatures:
         return lats, lons
 
     def _get_era5_single_levels_dataset(self, year: int, month: int) -> xr.Dataset:
-        if self.current_year_month == (year, month) and self.current_single_levels_ds is not None:
+        # start_Time = time.time()
+        if (
+            self.current_year_month_single == (year, month)
+            and self.current_single_levels_ds is not None
+        ):
+            log.info(f"Using cached dataset for {year} and {month} - single levels")
             return self.current_single_levels_ds
+        else:
+            pass
+            # log.error(f"Process: {current_process().name}")
+            # log.error(
+            #     f"single self.current_year_month_single == (year, month): {self.current_year_month_single == (year, month)}"
+            # )
+            # log.error(
+            #     f"single self.current_single_levels_ds is not None: {self.current_single_levels_ds is not None}"
+            # )
 
         era5_year_month_path = (
             self.era5_single_levels_path / "monthly_data" / f"RJ_{year}_{month}.nc"
@@ -101,12 +139,28 @@ class SpatioTemporalFeatures:
         #     ds = ds_combine
         ds = ds[["tp"]]
         self.current_single_levels_ds = ds
-        self.current_year_month = (year, month)
+        self.current_year_month_single = (year, month)
+        # end_Time = time.time()
+        # print(f"Time to load single levels dataset: {end_Time - start_Time}")
         return self.current_single_levels_ds
 
     def _get_era5_pressure_levels_dataset(self, year: int, month: int) -> xr.Dataset:
-        if self.current_year_month == (year, month) and self.current_pressure_levels_ds is not None:
+        # start_time = time.time()
+        if (
+            self.current_year_month_pressure == (year, month)
+            and self.current_pressure_levels_ds is not None
+        ):
+            log.info(f"Using cached dataset for {year} and {month} - pressure levels")
             return self.current_pressure_levels_ds
+        else:
+            pass
+            # log.error(f"Process: {current_process().name}")
+            # log.error(
+            #     f"pressure self.current_year_month_pressure == (year, month): {self.current_year_month_pressure == (year, month)}"
+            # )
+            # log.error(
+            #     f"pressure self.current_pressure_levels_ds is not None: {self.current_pressure_levels_ds is not None}"
+            # )
 
         era5_year_month_path = (
             self.era5_pressure_levels_path / "monthly_data" / f"RJ_{year}_{month}.nc"
@@ -124,7 +178,9 @@ class SpatioTemporalFeatures:
         #     ds = ds_combine
         ds = ds[["r", "t", "u", "v", "w"]]
         self.current_pressure_levels_ds = ds
-        self.current_year_month = (year, month)
+        self.current_year_month_pressure = (year, month)
+        # end_time = time.time()
+        # print(f"Time to load pressure levels dataset: {end_time - start_time}")
         return self.current_pressure_levels_ds
 
     def _process_grid(
@@ -139,8 +195,11 @@ class SpatioTemporalFeatures:
 
         processed = 0
         keys = []
+        # O(len(top_down_lats) * len(left_right_lons))
+        # O(n^2 * logn)
         for i, lat in enumerate(top_down_lats):
             for j, lon in enumerate(left_right_lons):
+                # O(logn), uses bisect
                 square = self.websirenes_square.get_square(
                     lat, lon, self.sorted_latitudes_ascending, self.sorted_longitudes_ascending
                 )
@@ -148,27 +207,35 @@ class SpatioTemporalFeatures:
                 if square is None:
                     continue
 
+                # O(83) ~ O(1)
                 websirene_keys = self.websirenes_square.get_keys_in_square(
                     square, self.stations_websirenes
                 )
+                # O(19) ~ O(1)
                 inmet_keys = self.inmet_square.get_keys_in_square(square, self.stations_inmet)
 
                 if inmet_keys or websirene_keys:
                     keys.append((i, j))
 
+                # O(websirene_keys) ~ O(83) ~ O(1)
                 tp = self.websirenes_square.get_precipitation_in_square(
                     square, websirene_keys, timestamp, ds_single_levels
                 )
 
+                # O(inmet_keys) ~ O(19) ~ O(1)
+                # Maybe call these two in a ThreadPoolExecutor?
                 tp_inmet = self.inmet_square.get_precipitation_in_square(
                     square, inmet_keys, timestamp, ds_single_levels
                 )
 
                 tp = max(tp, tp_inmet)
 
+                # O(4) ~ O(1)
                 r1000, r700, r200 = self.websirenes_square.get_relative_humidity_in_square(
                     square, ds_pressure_levels
                 )
+
+                # O(4) ~ O(1), all these below are the O(square)
                 t1000, t700, t200 = self.websirenes_square.get_temperature_in_square(
                     square, ds_pressure_levels
                 )
@@ -213,7 +280,85 @@ class SpatioTemporalFeatures:
         self.stations_cells.update(keys)
 
         total_squares = (len(top_down_lats) - 1) * (len(left_right_lons) - 1)
-        assert processed == total_squares, "Not all cells processed"
+        assert processed == total_squares, "Not all squares processed"
+
+        bottom_row_pressure_levels = ds_pressure_levels.sel(latitude=min(top_down_lats))
+        bottom_row_single_levels = ds_single_levels.sel(latitude=min(top_down_lats))
+
+        right_column_pressure_levels = ds_pressure_levels.sel(longitude=max(left_right_lons))
+        right_column_single_levels = ds_single_levels.sel(longitude=max(left_right_lons))
+
+        for j, lon in enumerate(left_right_lons):
+            features[-1, j] = [
+                bottom_row_single_levels["tp"].values[j] * 1000,
+                bottom_row_pressure_levels["r"].sel(pressure_level=200).values[j],
+                bottom_row_pressure_levels["r"].sel(pressure_level=700).values[j],
+                bottom_row_pressure_levels["r"].sel(pressure_level=1000).values[j],
+                bottom_row_pressure_levels["t"].sel(pressure_level=200).values[j],
+                bottom_row_pressure_levels["t"].sel(pressure_level=700).values[j],
+                bottom_row_pressure_levels["t"].sel(pressure_level=1000).values[j],
+                bottom_row_pressure_levels["u"].sel(pressure_level=200).values[j],
+                bottom_row_pressure_levels["u"].sel(pressure_level=700).values[j],
+                bottom_row_pressure_levels["u"].sel(pressure_level=1000).values[j],
+                bottom_row_pressure_levels["v"].sel(pressure_level=200).values[j],
+                bottom_row_pressure_levels["v"].sel(pressure_level=700).values[j],
+                bottom_row_pressure_levels["v"].sel(pressure_level=1000).values[j],
+                np.sqrt(
+                    bottom_row_pressure_levels["u"].sel(pressure_level=200).values[j] ** 2
+                    + bottom_row_pressure_levels["v"].sel(pressure_level=200).values[j] ** 2
+                ),
+                np.sqrt(
+                    bottom_row_pressure_levels["u"].sel(pressure_level=700).values[j] ** 2
+                    + bottom_row_pressure_levels["v"].sel(pressure_level=700).values[j] ** 2
+                ),
+                np.sqrt(
+                    bottom_row_pressure_levels["u"].sel(pressure_level=1000).values[j] ** 2
+                    + bottom_row_pressure_levels["v"].sel(pressure_level=1000).values[j] ** 2
+                ),
+                bottom_row_pressure_levels["w"].sel(pressure_level=200).values[j],
+                bottom_row_pressure_levels["w"].sel(pressure_level=700).values[j],
+                bottom_row_pressure_levels["w"].sel(pressure_level=1000).values[j],
+            ]
+            processed += 1
+
+        for i, lat in enumerate(top_down_lats):
+            features[i, -1] = [
+                right_column_single_levels["tp"].values[i] * 1000,
+                right_column_pressure_levels["r"].sel(pressure_level=200).values[i],
+                right_column_pressure_levels["r"].sel(pressure_level=700).values[i],
+                right_column_pressure_levels["r"].sel(pressure_level=1000).values[i],
+                right_column_pressure_levels["t"].sel(pressure_level=200).values[i],
+                right_column_pressure_levels["t"].sel(pressure_level=700).values[i],
+                right_column_pressure_levels["t"].sel(pressure_level=1000).values[i],
+                right_column_pressure_levels["u"].sel(pressure_level=200).values[i],
+                right_column_pressure_levels["u"].sel(pressure_level=700).values[i],
+                right_column_pressure_levels["u"].sel(pressure_level=1000).values[i],
+                right_column_pressure_levels["v"].sel(pressure_level=200).values[i],
+                right_column_pressure_levels["v"].sel(pressure_level=700).values[i],
+                right_column_pressure_levels["v"].sel(pressure_level=1000).values[i],
+                np.sqrt(
+                    right_column_pressure_levels["u"].sel(pressure_level=200).values[i] ** 2
+                    + right_column_pressure_levels["v"].sel(pressure_level=200).values[i] ** 2
+                ),
+                np.sqrt(
+                    right_column_pressure_levels["u"].sel(pressure_level=700).values[i] ** 2
+                    + right_column_pressure_levels["v"].sel(pressure_level=700).values[i] ** 2
+                ),
+                np.sqrt(
+                    right_column_pressure_levels["u"].sel(pressure_level=1000).values[i] ** 2
+                    + right_column_pressure_levels["v"].sel(pressure_level=1000).values[i] ** 2
+                ),
+                right_column_pressure_levels["w"].sel(pressure_level=200).values[i],
+                right_column_pressure_levels["w"].sel(pressure_level=700).values[i],
+                right_column_pressure_levels["w"].sel(pressure_level=1000).values[i],
+            ]
+            processed += 1
+        # the corner cell is processed twice, is the common point between the last row and the last column
+        processed -= 1
+        total_squares = len(top_down_lats) * len(left_right_lons)
+        assert (
+            processed == total_squares
+        ), "Not all cells processed failed to include last row and last column"
 
     def _process_timestamp(self, timestamp: pd.Timestamp):
         year = timestamp.year
@@ -229,32 +374,12 @@ class SpatioTemporalFeatures:
         ds_single_levels_time = ds_single_levels.sel(valid_time=time, method="nearest")
         ds_pressure_levels_time = ds_pressure_levels.sel(valid_time=time, method="nearest")
 
-        features_tuple = {
-            "tp": "Total precipitation",
-            "r200": "Relative humidity at 200 hPa",
-            "r700": "Relative humidity at 700 hPa",
-            "r1000": "Relative humidity at 1000 hPa",
-            "t200": "Temperature at 200 hPa",
-            "t700": "Temperature at 700 hPa",
-            "t1000": "Temperature at 1000 hPa",
-            "u200": "U component of wind",
-            "u700": "U component of wind",
-            "u1000": "U component of wind",
-            "v200": "V component of wind",
-            "v700": "V component of wind",
-            "v1000": "V component of wind",
-            "speed200": "Speed of wind at 200 hPa",
-            "speed700": "Speed of wind at 700 hPa",
-            "speed1000": "Speed of wind at 1000 hPa",
-            "w200": "Vertical velocity at 200 hPa",
-            "w700": "Vertical velocity at 700 hPa",
-            "w1000": "Vertical velocity at 1000 hPa",
-        }
+        # np.empty is faster but zeros is more explicity, easier to check if something went wrong, see validate_timestamps
         features = np.zeros(
             (
                 self.sorted_latitudes_ascending.size,
                 self.sorted_longitudes_ascending.size,
-                len(features_tuple),
+                len(self.features_tuple),
             ),
             dtype=np.float64,
         )
@@ -340,6 +465,10 @@ class SpatioTemporalFeatures:
         #         continue
 
         #     self._process_timestamp(timestamps[i])
+        # self.found_stations = self.stations_websirenes._getvalue()
+        # self.found_stations_inmet = self.stations_inmet._getvalue()
+        # self.stations_cells = self.stations_cells._getvalue()
+        # self.manager.shutdown()
         # end_time = time.time()
         # log.info(f"Target built in {end_time - start_time:.2f} seconds - sequential")
 
@@ -398,6 +527,21 @@ class SpatioTemporalFeatures:
                 not_found.append(timestamp)
                 continue
 
+            features = np.load(file)
+            assert (
+                features.shape[0] == len(self.sorted_latitudes_ascending)
+            ), f"shape[0] should be {len(self.sorted_latitudes_ascending)} but is {features.shape[0]}"
+            assert (
+                features.shape[1] == len(self.sorted_longitudes_ascending)
+            ), f"shape[1] should be {len(self.sorted_longitudes_ascending)} but is {features.shape[1]}"
+            assert features.shape[2] == len(
+                self.features_tuple
+            ), f"shape[2] should be {len(self.features_tuple)} but is {features.shape[2]}"
+
+            assert np.all(
+                np.any(features != 0, axis=tuple(range(1, features.ndim)))
+            ), f"Should not have one row with all values as zero for {file}"
+
             total_files += 1
 
         if not_found:
@@ -409,6 +553,15 @@ class SpatioTemporalFeatures:
         ), "Mismatch between timestamps and files (ignoring specific months)"
 
         log.success(
-            f"All timestamps found in target directory from {min_timestamp} to {max_timestamp}, ignoring months: {ignored_months}"
+            f"""All timestamps found in target directory:
+            From={min_timestamp}
+            To={max_timestamp}
+            Ignoring months={ignored_months}
+            Total timestamps={total_timestamps}
+            Shape: {features.shape}
+            All rows have at least one non-zero value: {np.all(
+                np.any(features != 0, axis=tuple(range(1, features.ndim)))
+            )}
+            """
         )
         return total_timestamps
